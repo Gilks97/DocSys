3767bac5274e59d1c5dc4757dff5b26c6c3e45de
Gilks97 <mosetig97@gmail.com>
2025-10-13 08:37:29 +0300

    settings statif files

--- docSys/settings.py
+++ docSys/settings.py
@@ -136,7 +136,7 @@ STATICFILES_DIRS = [
 ]
 
 # WhiteNoise configuration for static files
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
